2c28e5d3959d210dcfc8ca403a4670740cc52abd
caramel4764 <nina.dong4764@gmail.com>
2024-07-11 15:21:33 -0700

    working buttons + imported images

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-let leftButton =document.querySelector("#leftButton")
-let rightButton =document.querySelector("#rightButton")
+let leftButton = document.querySelector("#leftButton")
+let rightButton = document.querySelector("#rightButton")
+let skinTone = document.querySelector('#skinTone');
+function render(skin) {
+  if(skin==0){
+    skinTone.src = "./assets/skin1.PNG"
+  }
+  else if(skin==1){
+    skinTone.src = "./assets/skin2.PNG"
+  }
+  else if(skin==2){
+    skinTone.src = "./assets/skin2.PNG"
+  }
+  else if(skin==3){
+    skinTone.src = "./assets/skin3.PNG"
+  }
+  else if(skin==4){
+    skinTone.src = "./assets/skin4.PNG"
+  }
+  else if(skin==5){
+    skinTone.src = "./assets/skin5.PNG"
+  }
+}
+
 let skin=0
+render(skin)
 leftButton.addEventListener("click",function(){
-if(skin<1){
-  skin=5
-}
-else {
-  skin--
-}
+  if(skin<1){
+    skin=5
+  }
+  else {
+    skin--
+  }
+  render(skin)
+
 })
+
 rightButton.addEventListener("click",function(){
-  if(skin>5){
+  if(skin>4){
     skin=0
   }
   else {
     skin++
-  }
-  })
+  }  
+  render(skin)
+})
+
+  
+
