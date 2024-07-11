b421374b1b542b9e558024fa5ffd7ca0f926a68b
Caramel4764 <nina.dong4764@gmail.com>
2024-07-10 20:21:07 -0700

    test javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let leftButton =document.querySelector("#leftButton")
+let rightButton =document.querySelector("#rightButton")
+console.log(leftButton)
+leftButton.addEventListener("click",function(){
+console.log('hello')
+})
